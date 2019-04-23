46ddb7723797ba9cc87d4cc4b9f303bb430ce553
tpr1992 <terence.pataneronan@gmail.com>
2019-04-23 17:25:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+let num1 = 62
+let num2 = 1
+let random = Math.floor(Math.random() * 100 + 1)
+
 
 // Write your code here
+let multiply = num1 * num2
+
+let mod = 16 % 6
+let max = Math.max(2, 4, 10, 20)
