ff9e3ac0f2b7e4f9ea32668e08ff8eb3279022b0
beschwa <naraiswar@gmail.com>
2019-04-25 13:46:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
-// Write your code here
+// Write your code 
+
+let num1 = 2
+let num2 = 31
+const multiply = num1 * num2
+const random = Math.floor(Math.random() * 100) + 1
+const mod = 29 % 5
+const max = Math.max(4, 1, 5, 20)
+
+
